49e3040fbb32d6348b1cb7e921d41984d3fe89c4
luu <luu@forveri.io>
2024-09-24 08:18:28 +0200

    lang json and registger form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -53,7 +53,7 @@
             @csrf
               <div class="form-row">
                 <div class="col-md-4 ">
-                  <label for="validationCustom01">Company Name</label>
+                  <label for="validationCustom01">@lang('global.company_name')</label>
                   <div class="input-group">
                     <input type="text" class="form-control {{ $errors->has('company_name') ? ' is-invalid' : '' }}" id="validationCustom01"
                      placeholder="Tech Solutions LLC" name="company_name" required  value="{{ old('company_name') }}" >
@@ -63,7 +63,7 @@
                   </div>
                 </div>
                 <div class="col-md-4 ">
-                  <label for="validationCustom02">VAT Number</label>
+                  <label for="validationCustom02">@lang('VAT Number')</label>
                   <div class="input-group">
                     <input type="text" class="form-control {{ $errors->has('VAT') ? ' is-invalid' : '' }}" id="validationCustom02" 
                     placeholder="IT12345678901" name="VAT" required  value="{{ old('VAT') }}" >
@@ -73,7 +73,7 @@
                   </div>
                 </div>
                 <div class="col-md-4 ">
-                  <label for="validationCustom03">Contact Person's Name</label>
+                  <label for="validationCustom03">@lang("Contact Person's Name")</label>
                   <div class="input-group">
                     <input type="text" class="form-control {{ $errors->has('contact_person_name') ? ' is-invalid' : '' }}"
                      placeholder="Mario Rossi" name="contact_person_name" required  value="{{ old('contact_person_name') }}" >
@@ -86,7 +86,7 @@
               <div class="form-row">
                 <div class="col-md-4 ">
                   <div class="form-group">
-                    <label for="exampleSelect">Legal Form</label>
+                    <label for="exampleSelect">@lang('Legal Form')</label>
                     <select class="form-control" id="legal_form" name="legal_form">
                       <option value="1">LLC</option>
                       <option value="2">LTD</option>
@@ -98,7 +98,7 @@
                 </div>
                 <div class="col-md-4 ">
                   <div class="form-group">
-                    <label for="exampleSelect">Sector of Activity</label>
+                    <label for="exampleSelect">@lang('Sector of Activity')</label>
                     <select class="form-control" id="sector_activity" name="sector_activity">
                       <option value="1">Technology and IT</option>
                       <option value="2">Engineering</option>
@@ -110,7 +110,7 @@
                 </div>
                 <div class="col-md-4 ">
                   <div class="form-group">
-                    <label for="exampleSelect">Company Size</label>
+                    <label for="exampleSelect">@lang('Company Size')</label>
                     <select class="form-control" id="company_size" name="company_size">
                       <option value="1">1 ~ 9 employees</option>
                       <option value="2">10 ~ 50 employees</option>
@@ -123,7 +123,7 @@
               </div>
               <div class="form-row">
                 <div class="col-md-6 ">
-                  <label for="validationCustom03">Legal Address</label>
+                  <label for="validationCustom03">@lang('Legal Address')</label>
                   <div class="input-group">
                     <input type="text" class="form-control {{ $errors->has('legal_address') ? ' is-invalid' : '' }}" 
                     placeholder="10 Rome Street, 00100 Rome (RM)" name="legal_address" required  value="{{ old('legal_address') }}" >
@@ -134,7 +134,7 @@
                 </div>
               
                 <div class="col-md-6 ">
-                  <label for="validationCustom03">Operational Address(if different)</label>
+                  <label for="validationCustom03">@lang('Operational Address(if different)')</label>
                   <div class="input-group">
                     <input type="text" class="form-control {{ $errors->has('operational_address') ? ' is-invalid' : '' }}" 
                     placeholder="5 Milan Street, 00100 Rome (RM)" name="operational_address" value="{{ old('operational_address') }}" >
@@ -147,7 +147,7 @@
               </div>
               <div class="form-row">
                 <div class="col-md-6">
-                  <label for="validationCustom04">Position/Role</label> 
+                  <label for="validationCustom04">@lang('Position/Role')</label> 
                   <select class="form-control" id="position" name="position">                  
                     @foreach($roles as $role)
                       <option value="{{$role}}">{{trans($role)}}</option>;                  
@@ -155,7 +155,7 @@
                     </select>                  
                 </div>
                 <div class="col-md-6">
-                  <label for="validationCustom03">Business Email</label>
+                  <label for="validationCustom03">@lang('Business Email')</label>
                   <div class="input-group">
                     <input type="email" class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" 
                     id="validationCustom03" placeholder="Email Address" name="email" required  value="{{ old('email') }}" >
@@ -167,7 +167,7 @@
               </div>
               <div class="form-row">
                  <div class="col-md-6">
-                  <label for="validationCustom04">Business Mobile Phone</label>
+                  <label for="validationCustom04">@lang('Business Mobile Phone')</label>
                   <div class="input-group">
                     <input type="text" class="form-control {{ $errors->has('mobile_phone') ? ' is-invalid' : '' }}"
                      placeholder="+390123456789" name="mobile_phone" required  value="{{ old('mobile_phone') }}"
@@ -178,7 +178,7 @@
                   </div>
                 </div>
                 <div class="col-md-6">
-                  <label for="validationCustom04">Username</label>
+                  <label for="validationCustom04">@lang('global.login_name')</label>
                   <div class="input-group">
                     <input type="text" class="form-control {{ $errors->has('username') ? ' is-invalid' : '' }}" 
                     placeholder="tech_admin" name="username" required  value="{{ old('username') }}" >
@@ -191,7 +191,7 @@
               <div class="form-row">
                 
                 <div class="col-md-6">
-                  <label for="validationCustom04">Password </label>
+                  <label for="validationCustom04">@lang('global.login_password')</label>
                   <div class="input-group">
                     <input type="password" name="password"
                         class="form-input{{ $errors->has('password') ? ' is-invalid' : '' }}" required>
@@ -202,7 +202,7 @@
                   </div>
                 </div>
                 <div class="col-md-6">
-                  <label for="validationCustom04">Password Confirm</label>
+                  <label for="validationCustom04">@lang('global.login_password_confirmation')</label>
                   <div class="input-group">
                         <input type="password" name="password_confirmation" class="form-input" required>
                         @if ($errors->has('password_confirmation'))
@@ -216,12 +216,12 @@
                 <div class="form-check pt-2">
                   <label class="ms-checkbox-wrap" for="accept_terms_conditions">
                     <input class="form-check-input" type="checkbox" name="accept_terms" id="accept_terms_conditions" required>
-                    Accept to terms and conditions 
+                    @lang('Accept to terms and conditions')
                   </label>
                   <br>
                   <label class="ms-checkbox-wrap" for="accept_policy">
                     <input class="form-check-input" type="checkbox" name="accept_privacy" id="accept_policy" required>
-                    <span>Accept privacy policy</span><a href="/terms" class="btn-link">Terms and services</a>
+                    <span>@lang('Accept privacy policy')</span><a href="/terms" class="btn-link">@lang('Terms and services')</a>
                   </label>
                 </div>
               </div>            
@@ -238,7 +238,7 @@
 
             <div class="flex justify-center items-center mt-4">
                 <div>
-                    <a class="link" href="{{ route('login') }}">Sign In Here</a>
+                    <a class="link" href="{{ route('login') }}">@lang('Sign In Here')</a>
                 </div>
             </div>
 
